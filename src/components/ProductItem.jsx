beb8d24691afe25061a211def48b8266a129843e
Filipe Romano <filromano@gmail.com>
2020-07-16 14:33:45 -0300

    product item css

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -8,13 +8,28 @@ export default (props) => (
       <p><strong>Brahma</strong></p>
       <p>Cerveja Brahma 300ml</p>
     </div>
-    <div>imagem</div>
+    <div className="image">
+      <img src="https://cdn.shopify.com/s/files/1/0010/3150/3987/products/Enrrgetico_fusion_250ml_Lata.png?v=1565713637" alt="place"/>
+    </div>
     <div className="packs">
       <p>Escolha o seu pack</p>
+      <div className="options">
+        <button>15 Unid.</button>
+        <button>30 Unid.</button>
+        <button>60 Unid.</button>
+      </div>
     </div>
     <div className="prices">
-      <p>desconto vlavlavlavl</p>
+      <span><strong>DESC.:</strong>
+        <span className="discount"> 30%</span>
+      </span>
+      <span><strong>DE:</strong>
+        <span className="original-price"> R$ 68,70</span>
+      </span>
+      <span><strong>POR:</strong>
+        <span className="final-price"> R$ 53,70</span>
+      </span>
     </div>
-    <button>Adicionar ao carrinho</button>
+    <button className="cart">Adicionar ao carrinho</button>
   </div>
 );
