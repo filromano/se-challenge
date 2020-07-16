e19bd2b2068f092a1121f1acd684a4c7fe3779f3
Filipe Romano <filromano@gmail.com>
2020-07-16 12:58:14 -0300

    Begin

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import './style/index.css';
+
+export default (props) => (
+  <div className="test">App</div>
+)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin()
+  ]
+};
